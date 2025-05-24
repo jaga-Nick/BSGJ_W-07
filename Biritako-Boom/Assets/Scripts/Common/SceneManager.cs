3e7867e6be652e7a4d22cf494e8c8074b3371c4b
Asameshi <asakurashi00@icloud.com>
2025-05-25 00:52:16 +0900

    feat: Titleのボタンからのシーン遷移を実現

---
+++ Biritako-Boom/Assets/Scripts/Common/SceneManager.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+
+namespace Common
+{
+    /// <summary>
+    /// シーンマネージャーとして設計。
+    /// Addressableでの非同期読み込みとしてシーンを運用します。
+    /// </summary>
+    public class SceneManager : SingletonMonoBehaviourBase<SceneManager>
+    {
+        /// <summary>
+        /// メイン
+        /// </summary>
+        private AsyncOperationHandle<SceneInstance> _mainScene = default;
+        private ISceneInfo _mainSceneInfo = null;
+
+        /// <summary>
+        /// サブ（上書きで呼び出すシーン⇒メニュー等）
+        /// </summary>
+        private AsyncOperationHandle<SceneInstance> _subScene = default;
+        private ISceneInfo _subSceneInfo = null;
+
+
+        /// <summary>
+        /// 単一シーンロード:メイン
+        /// </summary>
+        /// <param name="mainSceneInfo"></param>
+        public async UniTask LoadMainScene(ISceneInfo mainSceneInfo)
+        {
+            if( _mainSceneInfo != null)
+            {
+                //終了処理
+                await _mainSceneInfo.End();
+                _mainSceneInfo.InputStop();
+            }
+        
+
+            //こちらでメインのロードを行う。
+            using (var _cts = new CancellationTokenSource())
+            {
+                //トークン発行
+                var token = _cts.Token;
+                try
+                {
+                    if (_subSceneInfo != null)
+                    {
+                        await UnloadSubScene();
+                    }
+
+                    //ロード
+                    _mainScene = Addressables.LoadSceneAsync(mainSceneInfo.SceneName,UnityEngine.SceneManagement.LoadSceneMode.Single);
+                    await _mainScene.ToUniTask(cancellationToken: token);
+                }
+                catch (OperationCanceledException)
+                {
+                    Debug.Log("シーンロードキャンセル");
+                }
+                finally 
+                {
+                    //メインシーンインスタンス更新
+                    _mainSceneInfo = mainSceneInfo;
+
+                    //初期化処理(ゲーム開始等の処理）
+                    _mainSceneInfo.InputStart();
+                    await _mainSceneInfo.Init();
+                }
+            }
+        }
+        
+        
+        /// <summary>
+        /// シーンロード：サブ用。（メニュー画面等）
+        /// </summary>
+        /// <param name="sceneInfo"></param>
+        /// <returns></returns>
+        public async UniTask LoadSubScene(ISceneInfo sceneInfo)
+        {
+            using (var cts = new CancellationTokenSource())
+            {
+                var token = cts.Token;
+            
+                try
+                {
+                    _subScene = Addressables.LoadSceneAsync(sceneInfo.SceneName,UnityEngine.SceneManagement.LoadSceneMode.Additive);
+                    await _subScene.ToUniTask(cancellationToken: token);
+                }
+                catch (OperationCanceledException)
+                {
+                    Debug.Log("シーンロードキャンセル");
+                }
+            }
+        }
+    
+        /// <summary>
+        /// サブシーンをアンロードする。
+        /// </summary>
+        public async UniTask UnloadSubScene()
+        {
+            if (_subSceneInfo!=null)
+            {
+                //サブシーンのActionMapを終了
+                _subSceneInfo?.InputStop();
+                //終了処理
+                await _subSceneInfo.End();
+
+                using (var _cts = new CancellationTokenSource()) { 
+
+                    var token = _cts.Token;
+                    try
+                    {
+                        //アンロード
+                        _subScene = Addressables.UnloadSceneAsync(_subScene);
+                        await _subScene.ToUniTask(cancellationToken: token);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        Debug.Log("シーンアンロードキャンセル");
+                    }
+                    finally
+                    {
+                        _subSceneInfo = null;
+                        _subScene=default;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 初期に呼び出す処理(メインシーン初期設定をここで行う）
+        /// </summary>
+        [RuntimeInitializeOnLoadMethod]
+        public static void Init()
+        {
+
+        }
+    }
+}
---
+++ Biritako-Boom/Assets/Scripts/Title/Button/ButtonEvent.cs
@@ -0,0 +1,46 @@
+﻿using Common;
+using Title.Loader;
+using UnityEngine;
+
+namespace Title.Button
+{
+    public class ButtonEvent : MonoBehaviour
+    {
+        private readonly ISceneInfo _settingSceneLoader = new SettingSceneLoader();
+        private readonly ISceneInfo _inGameSceneLoader = new InGameSceneLoader();
+        private ISceneInfo _introSceneLoader = new IntroSceneLoader();
+
+        /// <summary>
+        /// Settingボタンを押してSettingシーンに遷移する
+        /// </summary>
+        public async void OnClickSettingButton()
+        {
+            await SceneManager.Instance().LoadMainScene(_settingSceneLoader);
+        }
+
+        /// <summary>
+        /// Playボタンを押してInGameシーンに遷移する。一旦Introシーンはさむ。
+        /// </summary>
+        public async void OnClickPlayButton()
+        {
+            await SceneManager.Instance().LoadMainScene(_inGameSceneLoader);
+        }
+
+        /// <summary>
+        /// Creditボタンを押してCreditPanelを開く。
+        /// </summary>
+        public async void OnClickCreditButton()
+        {
+            // await SceneManager.Instance().LoadMainScene(_settingSceneLoader);
+        }
+
+        /// <summary>
+        /// Exitボタンを押してゲームを終了する。
+        /// </summary>
+        public void OnClickExitButton()
+        {
+            // Application.Quit();
+            Debug.Log("Exit");
+        }
+    }
+}
