44a8f19781f5469c489517cdb323e5d94bc7b601
jaga-Nick <shrimp1059yt@gmail.com>
2025-06-06 20:30:26 +0900

    ref : namespace(Result.Presenter)の追加

--- Biritako-Boom/Assets/Scripts/Result/Presenter/ResultPresenter.cs
+++ Biritako-Boom/Assets/Scripts/Result/Presenter/ResultPresenter.cs
@@ -3,43 +3,46 @@ using Cysharp.Threading.Tasks;
 using Result.Model;
 using Result.View;
 
-public class ResultPresenter : MonoBehaviour
+namespace Result.Presenter
 {
-    // ここはSerializeFieldでインスペクターから参照を設定
-    [SerializeField] private ResultScoreView _resultScoreView; 
-    [SerializeField] private ResultScoreModel _resultScoreModel = new ResultScoreModel(); 
-
-    private async void Start()
+    public class ResultPresenter : MonoBehaviour
     {
-        // Viewの初期化を指示
-        if (_resultScoreView != null)
+        // ここはSerializeFieldでインスペクターから参照を設定
+        [SerializeField] private ResultScoreView _resultScoreView; 
+        [SerializeField] private ResultScoreModel _resultScoreModel = new ResultScoreModel(); 
+    
+        private async void Start()
         {
-            _resultScoreView.Initialize();
+            // Viewの初期化を指示
+            if (_resultScoreView != null)
+            {
+                _resultScoreView.Initialize();
+            }
+    
+    
+            // Modelの初期化（ダミースコア生成）を指示
+            if (_resultScoreModel != null)
+            {
+                _resultScoreModel.ScoreGen();
+            }
+    
+            
+            await DisplaySingleScore(); // 1つのスコアを表示
         }
-
-
-        // Modelの初期化（ダミースコア生成）を指示
-        if (_resultScoreModel != null)
+    
+        /// <summary>
+        /// 1つのスコアを非同期で表示する
+        /// </summary>
+        public async UniTask DisplaySingleScore()
         {
-            _resultScoreModel.ScoreGen();
+            Debug.Log("Loading single score...");
+            
+            // Modelから1つのスコアを非同期で取得
+            int score = await _resultScoreModel.GetScore();
+    
+    
+            // Viewにスコアの表示アニメーションを指示
+            await _resultScoreView.DisplaySingleScoreAnimation(score);
         }
-
-        
-        await DisplaySingleScore(); // 1つのスコアを表示
-    }
-
-    /// <summary>
-    /// 1つのスコアを非同期で表示する
-    /// </summary>
-    public async UniTask DisplaySingleScore()
-    {
-        Debug.Log("Loading single score...");
-        
-        // Modelから1つのスコアを非同期で取得
-        int score = await _resultScoreModel.GetScore();
-
-
-        // Viewにスコアの表示アニメーションを指示
-        await _resultScoreView.DisplaySingleScoreAnimation(score);
     }
 }
