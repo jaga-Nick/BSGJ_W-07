8003ca6931a9fa8f2b524b6f692098107ae25ac1
Asameshi <asakurashi00@icloud.com>
2025-06-09 22:21:13 +0900

    feat: UFOの動きを修正

---
+++ Biritako-Boom/Assets/Scripts/InGame/Utility/RandomGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Random = UnityEngine.Random;
+
+namespace InGame.Utility
+{
+    public class RandomGenerator
+    {
+        public float DetermineRandomCoordinates(float min, float max)
+        {
+            float value;
+            do { value = Random.Range(min, max); } while (value is >= 0.0f and <= 1.0f);
+            return value;
+        }
+    }
+}
