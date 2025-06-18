b36b3b07c94506499607a5a45b5a6be00ff7506c
Asameshi <asakurashi00@icloud.com>
2025-06-19 02:25:37 +0900

    feat: TimerUIとの同期

--- Biritako-Boom/Assets/Scripts/InGame/NonMVP/GameManager.cs
+++ Biritako-Boom/Assets/Scripts/InGame/NonMVP/GameManager.cs
@@ -1,31 +1,36 @@
-﻿using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace InGame.NonMVP
 {
     /// <summary>
     /// ゲームの流れを管理するクラス。
+    /// UI周りもこちらで行う。
     /// </summary>
     public class GameManager : MonoBehaviour
     {
         /// <summary>
         /// ゲームタイムのパラメタ
         /// </summary>
-        [Header("ゲーム時間（分）")]
-        [SerializeField] private float inGameMinutes;
-        
+        [Header("ゲームタイムのパラメータ")]
+        [Header("ゲームの全体時間（秒）")]
+        [SerializeField] private float inGameSecond;
         
         /// <summary>
-        /// UI
+        /// uGUI
         /// </summary>
-        [SerializeField] private TextMeshProUGUI timerText;
+        [Header("タイマーのUI")]
+        [SerializeField] protected Image timerUI;
+        [Header("開始の色")]
+        [ColorUsage(false, false), SerializeField] private Color startColor;
+        [Header("終端の色")]
+        [ColorUsage(false, false), SerializeField] private Color endColor;
         // [SerializeField] private Button pauseButton;
         
         private void Start()
         {
             // ゲーム開始時はゲームタイムを0にする。
-            TimeManager.Instance().ResetInGameTime(inGameMinutes);
+            TimeManager.Instance().ResetInGameTime(inGameSecond);
         }
 
         /// <summary>
@@ -34,7 +39,19 @@ namespace InGame.NonMVP
         private void Update()
         {
             var time = TimeManager.Instance().GetInGameTime();
-            timerText.text = time.ToString("F2");
+            UpdateTimerUI(time);
+        }
+
+        /// <summary>
+        /// タイマーのUIの更新
+        /// </summary>
+        /// <param name="time"></param>
+        private void UpdateTimerUI(float time)
+        {
+            timerUI.fillAmount = time / inGameSecond;
+            // 時間経過とともに色を変えていく
+            var t = Mathf.Clamp01(time / inGameSecond);
+            timerUI.color = Color.Lerp(endColor, startColor, t);
         }
     }
 }
