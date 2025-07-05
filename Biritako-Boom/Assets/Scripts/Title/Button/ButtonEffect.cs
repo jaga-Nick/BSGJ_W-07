1b78349c6f07c9aed5555132ccd00bb0ab9f3adb
Asameshi <asakurashi00@icloud.com>
2025-07-05 22:54:07 +0900

    feat: ボタンUIの追加と9Sliceの導入

---
+++ Biritako-Boom/Assets/Scripts/Title/Button/ButtonEffect.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Title.Button
+{
+    /// <summary>
+    /// ボタンのエフェクト
+    /// </summary>
+    public class ButtonEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+    {
+        /// <summary>
+        /// animatorとbutton
+        /// </summary>
+        private Animator _animator;
+        private UnityEngine.UI.Button _button;
+        
+        /// <summary>
+        /// animatorのハッシュ化された状態
+        /// </summary>
+        private static readonly int Highlighted = Animator.StringToHash("Highlighted");
+        private static readonly int Normal = Animator.StringToHash("Normal");
+        private static readonly int Pressed = Animator.StringToHash("Pressed");
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _button = GetComponent<UnityEngine.UI.Button>();
+        }
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _animator.SetTrigger(Highlighted);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _animator.SetTrigger(Normal);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _animator.SetTrigger(Pressed);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _animator.SetTrigger(Highlighted);
+        }
+    }
+}
